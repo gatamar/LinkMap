104c1903e0b008fabd73061752c4f5e7b0f36301
gatamar <stub@gmail.com>
2024-02-24 23:58:37 +0100

    initial commit

---
+++ LinkMap/LinkMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  LinkMapApp.swift
+//  LinkMap
+//
+//  Created by Olha Bachalo on 24/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct LinkMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
